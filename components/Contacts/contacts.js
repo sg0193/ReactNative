f4e114bebd21dd559aecc85d11669d9ff18f6ff3
shravan gurijala <shravan.guri@gmail.com>
2018-10-08 19:57:24 -0500

    Adding Components to the app

---
+++ components/Contacts/contacts.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import {StyleSheet,Text,View } from 'react-native';
+
+export default class BoilerPlate extends Component<Props>  {
+    render(){
+            return (
+                <View>
+                    <Text>Sravan Gurijala</Text>
+                    <Text>Email: sravan.guri@gmail.com</Text>
+                </View>
+            );
+    }
+}
---
+++ components/photoButton/photoButton.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import {StyleSheet,Text,View } from 'react-native';
+
+export default class BoilerPlate extends Component<Props>  {
+    render(){
+            return (
+                <View>
+                    <Text>Sravan Gurijala</Text>
+                    <Text>Email: sravan.guri@gmail.com</Text>
+                </View>
+            );
+    }
+}
+
+const styles = StyleSheet.create({
+});
